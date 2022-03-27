508375842977be30682065db4501f3738da04cc0
cituk <ecituk@thefives.com.mx>
2022-03-27 16:20:28 -0500

    Cómo crear y renderizar componentes de Livewire

---
+++ resources/views/livewire/articles.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium quisquam omnis quo exercitationem laudantium porro enim animi corrupti dignissimos impedit perspiciatis reiciendis, aperiam, ipsam quaerat nesciunt delectus ex eius sequi.
+</div>
