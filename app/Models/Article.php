f19555e8dbeb9c44c467eb34f5857535b064c719
cituk <ecituk@thefives.com.mx>
2022-04-06 00:06:35 -0500

    Imagen por defecto para los artículos

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {
@@ -16,7 +17,18 @@ class Article extends Model
         return 'slug';
     }
 
+    public function imageUrl(){
+        
+        // return Storage::disk('public')->url($this->image);
+        return $this->image ? 
+            Storage::disk('local')->url($this->image) :
+            'https://via.placeholder.com/640x480.png/6366f1/FFFFFF?text=no-image'
+            ;
+
+    }
+
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
 }
--- resources/views/livewire/articles-table.blade.php
+++ resources/views/livewire/articles-table.blade.php
@@ -28,7 +28,7 @@
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="flex items-center">
                                                 <div class="flex-shrink-0 h-10 w-10">
-                                                    <img class="h-10 w-10 rounded-full" src="{{ $article->image }}" alt="{{ $article->title }}" />
+                                                    <img class="h-10 w-10 rounded-full" src="{{ $article->imageUrl() }}" alt="{{ $article->title }}" />
                                                 </div>
                                                 <div class="ml-4">
                                                     <div class="text-sm font-medium text-gray-900">
