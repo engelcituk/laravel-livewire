8d87ae52d46e47351d92b26193d0d46a73b171b1
cituk <ecituk@thefives.com.mx>
2022-04-07 22:52:26 -0500

    Cómo ordenar datos de una tabla con Livewire

--- app/Http/Livewire/ArticlesTable.php
+++ app/Http/Livewire/ArticlesTable.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Livewire\WithPagination;
-
+use App\Models\Article;
 
 class ArticlesTable extends Component
 {
@@ -18,11 +18,26 @@ class ArticlesTable extends Component
     use WithPagination;
 
     public $search = '';
+    public $sortField = 'created_at';
+    public $sortDirection = 'desc';
 
     public function render()
     {
         return view('livewire.articles-table',[
-            'articles' => \App\Models\Article::where('title', 'like', "%{$this->search}%")->latest()->paginate(5)
+            'articles' => Article::query()
+                ->where('title', 'like', "%{$this->search}%")
+                ->orderBy($this->sortField, $this->sortDirection)
+                ->paginate(5)
         ])->layout('layouts.app'); // ->layout('layouts.app'), se puede quitar, porque eso usa livewire por defecto
+    }
+
+    public function sortBy($field){
+
+        $this->sortField === $field
+            ? $this->sortDirection = $this->sortDirection === 'asc' ? 'desc': 'asc'
+            : $this->sortDirection = 'asc';
+        
+        $this->sortField = $field;
+
     }
 }
